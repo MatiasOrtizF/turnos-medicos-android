7571a396c2b4d14b3fd0a5d0b1283adc9f573345
matias <matias_ortiz_@outlook.es>
2024-05-13 23:29:04 -0300

    add login

---
+++ app/src/main/java/com/mfo/turnosmedicos/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.mfo.turnosmedicos.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mfo.turnosmedicos.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
